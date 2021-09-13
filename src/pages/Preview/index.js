ef574e5ffbb35000af063853072eb50c474ec3c6
maxim760 <maks.tomanov@mail.ru>
2021-09-13 13:06:30 +0300

    fix

--- src/pages/Preview/index.js
+++ src/pages/Preview/index.js
@@ -16,6 +16,7 @@ export const PreviewPage = () => {
   const onClickNextPage = () => {
     console.log('before')
     window.postMessage('mobile start', '*')
+    window.parent.postMessage('mobile start parent', '*')
     console.log('after')
     setIsStarted(true)
     history.push(ROUTES.MAIN)
