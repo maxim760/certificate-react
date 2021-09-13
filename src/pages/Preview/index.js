504a7b12c16186752259b64e962061928776673a
maxim760 <maks.tomanov@mail.ru>
2021-09-13 12:49:02 +0300

    post message

--- src/pages/Preview/index.js
+++ src/pages/Preview/index.js
@@ -14,6 +14,7 @@ export const PreviewPage = () => {
     }
   }, [])
   const onClickNextPage = () => {
+    window.postMessage('mobile start')
     setIsStarted(true)
     history.push(ROUTES.MAIN)
   }
